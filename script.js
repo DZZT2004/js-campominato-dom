0db0966e6f3053c71134eccb8e5478fae9d9a427
DZZT2004 <taodaiuwu@gmail.com>
2022-11-07 01:31:09 +0100

    esercizio finito

--- script.js
+++ script.js
@@ -11,19 +11,61 @@ bottone.classList.add("play");
 head.appendChild(titolo);
 head.appendChild(bottone);
 
+
 // Aggiunge evenet listener al bottone
 bottone.addEventListener("click", () =>{
     var quadrato = document.createElement("div");
+    var punti = 0;
+    const mine = [];
+    mina_pestata = false;
+    var punteggio = document.createElement("p");
+    var stringa;
     quadrato.classList.add("quadrato");
     contenitore.appendChild(quadrato);
+    // Creazione array
+    var arr = [];
+
+    for (var i = 1; i <= 100; i++) {
+        arr.push(true);
+    }
+    console.log(arr);
+    // Creazione mine
+    for (let index = 0; index < 16; index++) {
+        mine.push(Math.floor(Math.random() * 100) + 1);
+    }
+    console.log(mine);
+
     for (let index = 1; index < 101; index++) {
         let pezzo = document.createElement("div");
         pezzo.innerHTML = index;
         pezzo.classList.add("pezzo");
-        pezzo.addEventListener("click", () =>{
-            pezzo.style.backgroundColor = 'azure';
-            console.log(index);
-        })
+        if(mine.includes(index)){
+            pezzo.addEventListener("click", () =>{
+                if(!mina_pestata){
+                    pezzo.style.backgroundColor = 'red';
+                    console.log("Hai calpestato una mina, partita finita!");
+                    stringa = " Mi dispiace, hai perso, il tuo punteggio è:" + punti;
+                    punteggio.innerHTML = stringa;
+                    quadrato.appendChild(punteggio);
+                    mina_pestata = true;
+                }
+            })
+        }else{
+            pezzo.addEventListener("click", () =>{
+                if(arr[index-1] && !mina_pestata){
+                    pezzo.style.backgroundColor = 'azure';
+                    console.log(index);
+                    punti++;
+                    arr[index-1] = false;
+                }
+                if(punti == 84){
+                    punteggio = document.createElement("p");
+                    stringa = "Congratulazioni, hai vinto!";
+                    punteggio.innerHTML = stringa;
+                    quadrato.appendChild(punteggio);
+                }
+            })
+        }
         quadrato.appendChild(pezzo);
     }
     bottone.disabled = true;
